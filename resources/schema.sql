62239f5b895c586bcc023072dfebb727205e9f99
Jeremy Hinegardner <jeremy@hinegardner.org>
2007-09-12 07:50:06 +0000

    thinking about indexes
    20070912075006-2f820-1dc06f8a19879c20f174d4f60757be0dbe9ebfed.gz

--- resources/schema.sql
+++ resources/schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE imports (
 	id 						INTEGER PRIMARY KEY,
 	import_started_on		TIMESTAMP,	-- timestamp of when the run was started
 	import_ended_on			TIMESTAMP, 	-- timestamp of when the run was finished
+	import_date				TEXT, 		-- inserted with trigger
 	entry_started_on		TIMESTAMP,  -- timestamp in the first log entry processed
 	entry_ended_on			TIMESTAMP,	-- timestamp in the last log entry processed
 	entry_count				INTEGER, 	-- line count of rows entered into the database
@@ -10,16 +11,22 @@ CREATE TABLE imports (
 	end_offset				INTEGER, 	-- byte offset in the file of the ending pont of the run.
 	);
 	
+-- TODO: add index by import_date on imports
+	
 -- The basic information from every line of the log file
 CREATE TABLE log_entries (
 	id						INTEGER PRIMARY KEY,
 	import_id				INTEGER NOT NULL,
 	recorded_on				TIMESTAMP NOT NULL,
+	recorded_on_date		TEXT NOT NULL, -- inserted with trigger
 	hostname				TEXT NOT NULL,
 	process					TEXT NOT NULL,
 	pid						INTEGER,
 	message					TEXT NOT NULL
 	);
+-- TODO: add index by import_id
+-- TODO: add index by recorded_on_date
+
 	
 -- The messages that are  TCP logs
 CREATE TABLE tcp_log_messages (
@@ -27,48 +34,79 @@ CREATE TABLE tcp_log_messages (
 	log_entry_id			INTEGER NOT NULL,
 	client_address			TEXT NOT NULL,
 	client_port				INTEGER NOT NULL,
+	
 	recorded_on				TIMESTAMP NOT NULL,
+	recorded_on_date		TEXT NOT NULL, -- inserted with trigger
+	
 	frontend				TEXT NOT NULL,
 	backend					TEXT NOT NULL,
+	
 	queue_time				INTEGER NOT NULL, -- microseconds
 	connect_time			INTEGER NOT NULL, -- microseconds
 	total_time				INTEGER NOT NULL, -- microseconds
+	
 	bytes_read				INTEGER NOT NULL,
+	
 	termination_state		TEXT NOT NULL,
+	
 	active_sessions 		INTEGER NOT NULL,
 	frontend_connections	INTEGER NOT NULL, 
 	backend_connections 	INTEGER NOT NULL,
 	server_connections 		INTEGER NOT NULL,
+	
 	server_queue_size		INTEGER NOT NULL,
 	proxy_queue_size		INTEGER NOT NULL
 	);
+-- TODO: add index by log_entry_id
+-- TODO: add index by recorded_on_date
+-- TODO: add index by frontend
+-- TODO: add index by backend
+
 	
 -- The messages that are HTTP Logs	
-CREATE TABLE tcp_log_messages (
+CREATE TABLE http_log_messages (
 		id						INTEGER PRIMARY KEY,
 		log_entry_id			INTEGER NOT NULL,
 		client_address			TEXT NOT NULL,
 		client_port				INTEGER NOT NULL,
+		
 		recorded_on				TIMESTAMP NOT NULL,
+		recorded_on_date		TEXT NOT NULL, -- inserted with trigger
+		
 		frontend				TEXT NOT NULL,
 		backend					TEXT NOT NULL,
+		server					TEXT NOT NULL,
+		
 		request_time			INTEGER NOT NULL, -- microseconds
 		queue_time				INTEGER NOT NULL, -- microseconds
 		connect_time			INTEGER NOT NULL, -- microseconds
 		response_time			INTEGER NOT NULL, -- microseconds
 		total_time				INTEGER NOT NULL, -- microseconds
+		
 		http_status				INTEGER NOT NULL,
 		bytes_read				INTEGER NOT NULL,
+		
 		request_cookie			TEXT,
 		response_cookie			TEXT,
+		
 		termination_state		TEXT NOT NULL,
+		
 		active_sessions 		INTEGER NOT NULL,
 		frontend_connections	INTEGER NOT NULL, 
 		backend_connections 	INTEGER NOT NULL,
 		server_connections 		INTEGER NOT NULL,
+		
 		incoming_queue_size		INTEGER NOT NULL,
 		server_queue_size		INTEGER NOT NULL,
+		
 		request_headers			TEXT,
 		response_headers		TEXT,
+		
 		http_request			TEXT NOT NULL
 		);
+-- TODO: add index by log_entry_id
+-- TODO: add index by recorded_on_date
+-- TODO: add index by frontend
+-- TODO: add index by backend
+-- TODO: add index by server
+-- TODO: add index by status
